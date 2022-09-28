56d77fd397e0eeb92d02a06d7cf778f1f83e2fd4
keemsora <itsyoursora@gmail.com>
2022-09-28 16:59:32 +0900

    feat: TodoVO 객체 추가

---
+++ src/main/java/org/zerock/w1/domain/TodoVO.java
@@ -0,0 +1,21 @@
+package org.zerock.w1.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@ToString
+public class TodoVO {
+
+    private Long tno;
+
+    private String title;
+
+    private LocalDate dueDate;
+
+    private boolean finished;
+}
