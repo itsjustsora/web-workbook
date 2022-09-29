4e15dcdd31ca1fbc07e1d29fb3d0d7946f9bf7e4
keemsora <itsyoursora@gmail.com>
2022-09-29 23:53:59 +0900

    feat: 세션을 이용한 로그인 체크 구현

--- src/main/java/org/zerock/w1/controller/TodoRegisterController.java
+++ src/main/java/org/zerock/w1/controller/TodoRegisterController.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -22,7 +23,24 @@ public class TodoRegisterController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        log.info("/todo/register GET ...");
+
+        HttpSession session = req.getSession();
+
+        // JSESSIONID가 없다면
+        if (session.isNew()) {
+            log.info("JSESSIONID 쿠키가 새로 만들어진 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        // JSESSIONID는 있지만 세션 컨텍스트에 loginInfo 객체가 없는 경우
+        if (session.getAttribute("loginInfo") == null) {
+            log.info("로그인한 정보가 없는 사용자");
+            resp.sendRedirect("/login");
+            return;
+        }
+
+        // 일반적인 경우라면
         req.getRequestDispatcher("/WEB-INF/todo/register.jsp").forward(req, resp);
     }
 
